816f3630c8e7cac9de1c59eeee994257cd65aed5
Adam Goode <adam@spicenitz.org>
2018-09-30 21:45:56 -0400

    Update for 2018 draft

--- 2018/scripts-export.sql
+++ 2018/scripts-export.sql
@@ -6,12 +6,14 @@ drop table if exists teachers;
 .mode csv
 .import teachers.csv teachers
 
--- import
+-- responses
 .mode csv
 .import Ohara_pto_9_27_2018.csv responses
 
+-- export view
 create view export as
  select
+  responses.rowid + 1 as "no",
   "Student First Name" AS "first",
   "Student Last Name" AS "last",
   "Student Grade Level" AS "grade",
@@ -21,37 +23,37 @@ create view export as
   "Student City" AS "city",
   "Student State" AS "state",
   "Student Zip" AS "zip",
-  "Student Home Phone" AS "homephone",
-  "Parent/Guardian 1 First name" AS "pg1first",
-  "Parent/Guardian 1 Last Name" AS "pg1last",
-  "Parent/Guardian 1 Home Phone" AS "pg1home",
-  "Parent/Guardian 1 Mobile Phone" AS "pg1mobile",
-  "Parent/Guardian 1 Email" AS "pg1email",
-  "Parent/Guardian 2 First name" AS "pg2first",
-  "Parent/Guardian 2 Last Name" AS "pg2last",
-  "Parent/Guardian 2 Home Phone" AS "pg2home",
-  "Parent/Guardian 2 Mobile Phone" AS "pg2mobile",
-  "Parent/Guardian 2 Email" AS "pg2email",
-  "Parent/Guardian 3 First name" AS "pg3first",
-  "Parent/Guardian 3 Last Name" AS "pg3last",
-  "Parent/Guardian 3 Home Phone" AS "pg3home",
-  "Parent/Guardian 3 Mobile Phone" AS "pg3mobile",
-  "Parent/Guardian 3 Email" AS "pg3email"
+  "Student Home Phone" AS "home0",
+  -- pg1
+  trim(printf('%s %s', "Parent/Guardian 1 First name",
+                       "Parent/Guardian 1 Last Name")) AS "pg1",
+  "Parent/Guardian 1 Home Phone" AS "home1",
+  "Parent/Guardian 1 Mobile Phone" AS "cell1",
+  "Parent/Guardian 1 Email" AS "email1",
+  -- pg2
+  trim(printf('%s %s', "Parent/Guardian 2 First name",
+                       "Parent/Guardian 2 Last Name")) AS "pg2",
+  "Parent/Guardian 2 Home Phone" AS "home2",
+  "Parent/Guardian 2 Mobile Phone" AS "cell2",
+  "Parent/Guardian 2 Email" AS "email2",
+  -- pg3
+  trim(printf('%s %s', "Parent/Guardian 3 First name",
+                       "Parent/Guardian 3 Last Name")) AS "pg3",
+  "Parent/Guardian 3 Home Phone" AS "home3",
+  "Parent/Guardian 3 Mobile Phone" AS "cell3",
+  "Parent/Guardian 3 Email" AS "email3"
  from responses JOIN teachers USING ("teacher");
 
--- export
+-- run export
 .header on
 .mode ascii
-.once responses-20180927.list
+.once export-20180927.list
 select * from export order by rowid;
-
-.header on
 .mode csv
 .once export-20180927.csv
-select * from export
-  order by grade, teacher, last, first;
+select * from export order by grade, teacher, last, first;
 
--- students
+-- run student export
 .header on
 .mode ascii
 .once students-20180927.list
