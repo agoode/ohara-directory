dc80354c1a37efd9662899ebe46a2cf256eea1fc
Adam Goode <adam@spicenitz.org>
2019-10-10 21:50:22 -0400

    Get most of 2019 directory going

--- 2019/scripts-export.sql
+++ 2019/scripts-export.sql
@@ -8,57 +8,58 @@ drop table if exists teachers;
 
 -- responses
 .mode csv
-.import "OH-PTO-Directory 2.csv" responses
+.import "Copy of PTO-Directory-OH-100919.csv" responses
 
 -- export view
 create view export as
 with
   t as (select
-          trim(substr(student, 1, instr(student, ',')-1)) as last,
-          trim(substr(student, instr(student, ',')+1)) as first,
-          grade,
+          trim(substr("Stu Lastfirst", 1, instr("Stu Lastfirst", ',')-1)) as last,
+          trim(substr("Stu Lastfirst", instr("Stu Lastfirst", ',')+1)) as first,
+          "Stu Grade Level" as grade,
           teacher,
-          "contact priority order" as contact_order,
-          trim(substr("contact full name", instr("contact full name", '. ')+1)) as contact_name,
-          phone as contact_phone,
-          email as contact_email,
-          replace(replace(address, ',', ' '), '  ', ' ') as contact_address
-        from responses left outer join teachers using (homeroom)),
+          "contact priority order" as corder,
+          --trim(substr("contact full name", instr("contact full name", '. ')+1)) as cname,
+          "contact full name" as cname,
+          phone as cphone,
+          email as cemail,
+          replace(replace(address, ',', ' '), '  ', ' ') as caddress
+        from responses left outer join teachers using ("Stu homeroom")),
   t1 as (select
           last,
           first,
           grade,
           teacher,
-          contact_name as contact_name1,
-          contact_phone as contact_phone1,
-          contact_email as contact_email1,
-          contact_address as contact_address1
-         from t where contact_order = 1),
+          cname as cname1,
+          cphone as cphone1,
+          cemail as cemail1,
+          caddress as caddress1
+         from t where corder = 1),
   t2 as (select
           last,
           first,
           grade,
           teacher,
-          contact_name as contact_name2,
-          contact_phone as contact_phone2,
-          contact_email as contact_email2,
-          contact_address as contact_address2
-         from t where contact_order = 2)
-select distinct * from t1 join t2 using (last, first, teacher, grade);
+          cname as cname2,
+          cphone as cphone2,
+          cemail as cemail2,
+          caddress as caddress2
+         from t where corder = 2)
+select distinct * from t1 left outer join t2 using (last, first, teacher, grade);
 
 -- run export
 .header on
 .mode ascii
-.once export-20191008.list
+.once export-20191010.list
 select * from export order by last, first, grade, teacher;
 .mode csv
-.once export-20191008.csv
+.once export-20191010.csv
 select * from export order by grade, teacher, last, first;
 
 -- run student export
 .header on
 .mode ascii
-.once students-20191008.list
+.once students-20191010.list
 select distinct
   replace(grade, 'K', '0') as grade, TEACHER, first, last
   from export order by 1,2,4,3;
